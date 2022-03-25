a31b89340536707868818ce8fcbd10b7533e2734
georgemaganzini <involver7@gmail.com>
2022-03-25 10:11:38 -0700

    Add flatlist for blogPosts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,7 +4,14 @@ const BlogContext = React.createContext();
 
 
 export const BlogProvider = ({children}) => {
-    return <BlogContext.Provider>
+    const blogPosts = [
+        { title: 'Blog Post #1'},
+        { title: 'Blog Post #2'}
+    ];
+
+    return <BlogContext.Provider value={blogPosts}>
         {children}
     </BlogContext.Provider>
 };
+
+export default BlogContext;
